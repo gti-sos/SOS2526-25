5ac2bd91112e01ac449ef9f86d5c600fb02a0522
Pablo Seco Amores <accsecunadry@gmail.com>
2026-02-18 11:55:39 +0100

    Entregable 3 Index

---
+++ index-PSA.js
@@ -0,0 +1,22 @@
+let data = [
+    { country: "Germany", year: 2021, co2_emission: 679, precipitation: 772.59, temperature: 9.48 },
+    { country: "Austria", year: 2021, co2_emission: 65.5, precipitation: 1090, temperature: 7.29 },
+    { country: "Belgium", year: 2017, co2_emission: 99.1, precipitation: 876.48, temperature: 11.02 },
+    { country: "Italy", year: 2017, co2_emission: 353, precipitation: 685.54, temperature: 13.51 },
+    { country: "Turkey", year: 2022, co2_emission: 481, precipitation: 536.14, temperature: 12.24 },
+    { country: "Turkey", year: 2021, co2_emission: 467, precipitation: 559.24, temperature: 12.56 },
+    { country: "Mexico", year: 2020, co2_emission: 423, precipitation: 691.32, temperature: 22.21 },
+    { country: "United Kingdom", year: 2018, co2_emission: 380, precipitation: 1100, temperature: 9.53 },
+    { country: "Germany", year: 2015, co2_emission: 780, precipitation: 654.88, temperature: 10.33 },
+    { country: "China", year: 2019, co2_emission: 11.8, precipitation: 615.63, temperature: 8.04 },
+    { country: "China", year: 2021, co2_emission: 12.7, precipitation: 654.33, temperature: 8.23 }
+];
+
+function average_data(data){
+    let Germany = data.filter( (n) => n.country === "Germany");
+    let co2 = Germany.map( (n) => n.co2_emission);
+    let numerator = co2.reduce( (n, b) => n +=b);
+    return numerator/co2.length;
+}
+
+console.log(average_data(data))
