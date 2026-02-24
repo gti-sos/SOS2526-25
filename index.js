59bbfb8ddc764c48f9c77270860efead923e4a66
Juanlu <jlraparticular@gmail.com>
2026-02-24 22:20:55 +0100

    AboutCorrection

--- index.js
+++ index.js
@@ -4,11 +4,12 @@ const cool = require("cool-ascii-faces");
 
 const port = process.env.PORT || 8080; 
 
-// Ruta de prueba inicial
 app.get("/", (req, res) => {
     res.send("¡Hola! El servidor del Grupo 25 está funcionando 🚀");
 });
-app.use("/about", express.static("public"))
+app.get("/about", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "about.html"));
+});
 app.get("/cool", (req, res) => {
     res.send("<html><body><h1>" + cool() + "</h1></body></html>");
 });
