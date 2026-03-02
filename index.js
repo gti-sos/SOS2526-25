96ac75aaae5fb941287434bd22b776ed322f9a61
Juanlu <jlraparticular@gmail.com>
2026-03-02 12:12:21 +0100

    Backup

--- index.js
+++ index.js
@@ -133,57 +133,6 @@ app.get("/cool", (req, res) => {
     res.status(200).send("<html><body><h1>" + cool() + "</h1></body></html>");
 });
 
-
- 
-
-// --- DATOS Y ALGORITMO (PSA) ---
-
-const data_psa = [
-    { country: "Germany", year: 2021, co2_emission: 679, precipitation: 772.59, temperature: 9.48 },
-    { country: "Austria", year: 2021, co2_emission: 65.5, precipitation: 1090, temperature: 7.29 },
-    { country: "Belgium", year: 2017, co2_emission: 99.1, precipitation: 876.48, temperature: 11.02 },
-    { country: "Italy", year: 2017, co2_emission: 353, precipitation: 685.54, temperature: 13.51 },
-    { country: "Turkey", year: 2022, co2_emission: 481, precipitation: 536.14, temperature: 12.24 },
-    { country: "Turkey", year: 2021, co2_emission: 467, precipitation: 559.24, temperature: 12.56 },
-    { country: "Mexico", year: 2020, co2_emission: 423, precipitation: 691.32, temperature: 22.21 },
-    { country: "United Kingdom", year: 2018, co2_emission: 380, precipitation: 1100, temperature: 9.53 },
-    { country: "Germany", year: 2015, co2_emission: 780, precipitation: 654.88, temperature: 10.33 },
-    { country: "China", year: 2019, co2_emission: 11.8, precipitation: 615.63, temperature: 8.04 },
-    { country: "China", year: 2021, co2_emission: 12.7, precipitation: 654.33, temperature: 8.23 }
-];
-
-function average_data_psa(data) {
-    const germany_data = data.filter((n) => n.country === "Germany");
-    const co2_values = germany_data.map((n) => n.co2_emission);
-    // Calculamos la suma de las emisiones
-    const total_co2 = co2_values.reduce((acc, current) => acc + current, 0);
-    return total_co2 / co2_values.length;
-}
-
-// --- RUTAS INDEX DE PABLO SECO
-
-app.get("/", (req, res) => {
-    res.send("¡Hola! El servidor del Grupo 25 está funcionando 🚀");
-});
-
-app.get("/about", (req, res) => {
-    res.sendFile(path.join(__dirname, "public", "about.html"));
-});
-
-app.get("/cool", (req, res) => {
-    res.send("<html><body><h1>" + cool() + "</h1></body></html>");
-});
-
-// NUEVA RUTA SOLICITADA PARA EL ENTREGABLE F03
-app.get("/samples/PSA", (req, res) => {
-    const result = average_data_psa(data_psa);
-    res.send(`<html><body><p>The average CO2 emission for Germany is: ${result}</p></body></html>`);
-});
-
-app.listen(port, () => {
-    console.log(`Servidor corriendo en el puerto ${port}`);
-});
-
 // ==========================================
 // ARRANQUE DEL SERVIDOR
 // ==========================================
