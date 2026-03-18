3b1545e0f8daa7f26d59325e1f27be198e9db72a
Pablo Seco Amores <accsecunadry@gmail.com>
2026-03-18 12:09:44 +0100

    G25

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,11 @@ import { defineConfig } from 'vite';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
 
 export default defineConfig({
-    plugins: [svelte()]
+    plugins: [svelte()],
+    server: {
+        proxy: {
+            // Toda petición a /api se redirige a tu backend de Express
+            '/api': 'http://localhost:8082' 
+        }
+    }
 });
