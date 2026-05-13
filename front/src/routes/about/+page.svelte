80dd1733aa4a9d641c69267f7a177b2fcdeada43
Juanlu <jlraparticular@gmail.com>
2026-05-13 23:04:40 +0200

    Fix: Add "Sobre nuestro trabajo" link to main page and update video link on about page

--- front/src/routes/about/+page.svelte
+++ front/src/routes/about/+page.svelte
@@ -12,7 +12,7 @@
             <h3>Juan Luis Rodríguez</h3>
             <p>Datos: social-drinking-behaviors</p>
             <div class="video-container">
-                <iframe src="https://www.youtube.com/embed/ID_DEL_VIDEO" title="Vídeo Juan Luis" allowfullscreen></iframe>
+                <iframe src="https://youtu.be/WR0GNDIub8o" title="Vídeo Juan Luis" allowfullscreen></iframe>
             </div>
             <a href="https://youtu.be/G8kIyDLSDHI" target="_blank" class="btn">🔗 Ver en YouTube</a>
         </div>
